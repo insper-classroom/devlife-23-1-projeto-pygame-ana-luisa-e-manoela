59b58208db19f49ea6da6bdefbfd1935d7e1eed9
anayagi <anaayagi@gmail.com>
2023-04-20 15:59:20 -0300

    funcao inicializa

--- jogo/funcoes.py
+++ jogo/funcoes.py
@@ -1,1 +1,12 @@
 import pygame
+
+def inicializa():
+    assets = {
+
+    }
+    pygame.init()
+    window = pygame.display.set_mode((1000, 450), vsync=True, flags=pygame.SCALED)
+    state = {
+
+    }
+    return assets, window, state
