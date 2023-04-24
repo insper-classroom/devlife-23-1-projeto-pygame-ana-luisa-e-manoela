f844171990a72774089d0d048859f7e958f103d6
saragocams <saragocams@gmail.com>
2023-04-24 09:59:28 -0300

    adicionei brock e uma caixa para teste

--- jogo/funcoes.py
+++ jogo/funcoes.py
@@ -3,18 +3,27 @@ pygame.init()
 
 def inicializa():
     assets = {
+        'brock' : pygame.image.load("docs\imagens\personagem.png"), 
+        'caixas' : pygame.image.load("docs\imagens\caixa angry birds.png")
 
     }
+
     window = pygame.display.set_mode((1000, 450), vsync=True, flags=pygame.SCALED)
     state = {
-        'pos_brock': [990, 440], 
+
     }
+
     return window, assets, state
 
 def desenha(window, assets, state):
     fundo_jogo = pygame.image.load("docs\imagens\download.png")
     fundo_jogo = pygame.transform.scale(fundo_jogo, (1000, 450))
+    assets['brock'] = pygame.transform.scale(assets['brock'], (120,120))
+    assets['caixas'] = pygame.transform.scale(assets['caixas'], (60, 60)) 
     window.blit(fundo_jogo, (0,0)) 
+    window.blit(assets['brock'], ((5, 250)))
+    window.blit(assets['caixas'], ((800, 285)))
+
     pygame.display.update()
 
 def recebe_eventos():
