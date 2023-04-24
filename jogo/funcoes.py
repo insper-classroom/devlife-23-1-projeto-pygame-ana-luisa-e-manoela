028f20fa089b0fcceb12aa2106d466f5e7057349
saragocams <saragocams@gmail.com>
2023-04-24 08:55:03 -0300

    fiz o fundo do jogo

--- jogo/funcoes.py
+++ jogo/funcoes.py
@@ -1,17 +1,29 @@
 import pygame
-import os
-
-
-
-
+pygame.init()
 
 def inicializa():
     assets = {
 
     }
-    pygame.init()
     window = pygame.display.set_mode((1000, 450), vsync=True, flags=pygame.SCALED)
     state = {
-
+        'pos_brock': [990, 440], 
     }
-    return assets, window, state
+    return window, assets, state
+
+def desenha(window, assets, state):
+    fundo_jogo = pygame.image.load("docs\imagens\download.png")
+    fundo_jogo = pygame.transform.scale(fundo_jogo, (1000, 450))
+    window.blit(fundo_jogo, (0,0)) 
+    pygame.display.update()
+
+def recebe_eventos():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit() 
+
+if __name__ == '__main__':
+    window, assets, state = inicializa()
+    desenha(window, assets, state)
+    recebe_eventos()
